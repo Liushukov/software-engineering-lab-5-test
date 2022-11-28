2c8f403fbc673460a13bc1fabf7933cf3af6a856
Liushukov <artem.lushukov@gmail.com>
2022-11-28 21:15:40 +0200

    Init application

---
+++ Test.cpp
@@ -0,0 +1,71 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\NTU_KHPI\Инженерия_Програмного_Обеспечения\Lab3\IPZ_3\IPZ_3\IPZ_3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int n = 6;
+			int x = 2;
+			int expected = 0;
+
+			int actual = calculate(x, n);
+
+			Assert::AreEqual(expected, actual);
+		}
+
+	public:
+
+		TEST_METHOD(TestMethod2)
+		{
+			int n = 3;
+			int x = 2;
+			int expected = -7;
+
+			int actual = calculate(x, n);
+
+			Assert::AreEqual(expected, actual);
+		}
+	};
+	TEST_CLASS(checkValidParams_Tests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod3)
+		{
+			int n = -5;
+
+			try 
+			{
+				checkValidParams(n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	public:
+		TEST_METHOD(TestMethod4)
+		{
+			int n = 1;
+
+			try
+			{
+				checkValidParams(n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+}
---
+++ main.cpp
@@ -0,0 +1,112 @@
+﻿#include <Windows.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <clocale>
+using namespace std;
+
+void checkValidInput()
+{
+    if (cin.fail())
+    {
+        throw "Incorrect input";
+    }
+}
+
+void checkValidParams(int n)
+{
+
+    if (n <= 1)
+    {
+        throw "Input correct data";
+    }
+}
+void checkValidParams(int start, int end) {
+    if (start > end) {
+        throw "Incorrect input";
+    }
+}
+
+void checkValidParams(int start, int end, int step) {
+    if (step <= 0) {
+        throw "Incorrect input";
+    }
+}
+
+
+
+int calculate(int x, int n)
+{
+
+    if (x < 0)
+    {
+
+        int y = 1;
+        for (int i = 1; i <= (n - 1); i++)
+        {
+            int s = 0;
+            for (int j = 0; j <= n; j++) s = i / ((j * j) - x);
+            y *= s;
+        }
+        return y;
+    }
+    else
+    {
+        int s = 0;
+        for (int i = 1; i <= (n - 1); i++)
+        {
+            s += (i * i * i);
+        }
+        int y = x - s;
+        return y;
+    }
+}
+
+void print(int start, int end, int step, int n) {
+    for (int x = start; x <= end; x += step)
+    {
+        int y = calculate(x, n);
+        cout << "X: " << x << ";  Y: " << y << endl;
+    }
+}
+
+
+int main()
+{
+    int start, end, step, n;
+
+    try
+    {
+        cout << "start: ";
+        cin >> start;
+        checkValidInput();
+
+        cout << "end: ";
+        cin >> end;
+        checkValidInput();
+        checkValidParams(start, end);
+
+        cout << "step: ";
+        cin >> step;
+        checkValidInput();
+        checkValidParams(start, end, step);
+
+        cout << "n: ";
+        cin >> n;
+        checkValidInput();
+        checkValidParams(n);
+
+        print(start, end, step, n);
+    }
+    catch (const char* ex)
+    {
+        cout << ex << endl;
+        return -1;
+    }
+    catch (...)
+    {
+        cout << "Unknown Error" << endl;
+        return -2;
+    }
+}
